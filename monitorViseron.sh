d63dbb09fe5fb696818937175ddc82fafd99c3d4
Bioyina <rolf.moser@gmail.com>
2023-10-22 08:48:50 +0200

    restart container when camera_1 is dead

--- monitorViseron.sh
+++ monitorViseron.sh
@@ -3,11 +3,26 @@
 # File path to the script itself
 SCRIPT_PATH=$(realpath "$0")
 
-# Function for monitoring
+# Function for monitoring the website
 monitor_docker() {
     # Check the website
     if ! curl -s "http://nano.fqp.ch:8888/" > /dev/null; then
-        echo "$(date) - Connection failed. Restarting 'viseron' container." >> ./viseron_monitor.log
+        echo "$(date) - Connection failed. Restarting viseron container." >> ./viseron_monitor.log
+        docker restart viseron
+    fi
+}
+
+# Function for monitoring the logs
+function check_and_restart_viseron() {
+    # Get the last line from the Docker logs for the viseron container
+    local last_log=$(docker logs viseron 2>&1 | tail -n 1)
+
+    # Desired string to match
+    local match_str="[viseron.watchdog.thread_watchdog] - Thread viseron.camera.camera_1 is dead, restarting"
+
+    # Check if the last_log matches the desired string
+    if [[ "$last_log" == *"$match_str"* ]]; then
+        echo "Thread viseron.camera.camera_1 is dead, restarting viseron container..." >> ./viseron_monitor.log
         docker restart viseron
     fi
 }
